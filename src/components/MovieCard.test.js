7459aa35b95f31296046053f56b59fe3acc41125
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-10-26 11:19:51 +0530

    Added test for title and subheader.

--- src/components/MovieCard.test.js
+++ src/components/MovieCard.test.js
@@ -3,14 +3,30 @@ import {shallow} from 'enzyme';
 import {MovieCard} from './MovieCard';
 import CardHeader from '@material-ui/core/CardHeader';
 
-it("should render the movie title", () => {
+describe("movie card header", () =>{
   const movie = {
     title: "Gunda",
     snippet: "Greatest movie of all time."
   };
-  let wrapper = shallow(<MovieCard movie={movie}/>);
+  var shallowWrapper;
+  beforeEach(() => {
+    shallowWrapper = shallow(<MovieCard movie={movie}/>);
+  })
+  it("should contain a card header", () => {
+    
+  
+    expect(shallowWrapper.find(CardHeader).exists()).toBe(true);
+  });
 
-  expect(wrapper.find(CardHeader).exists()).toBe(true);
+  it("the card header should contain movie title.", () => {
+    const cardHeaderWrapper = shallowWrapper.find(CardHeader);
+    expect(cardHeaderWrapper.props().title).toEqual("Gunda");
+  })
+
+  it("the card header should contain movie subtitle.", () => {
+    const cardHeaderWrapper = shallowWrapper.find(CardHeader);
+    expect(cardHeaderWrapper.props().subheader).toEqual("Greatest movie of all time.");
+  })
+  
 });
-
 
