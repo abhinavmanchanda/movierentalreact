b5d6f3597f3193ea9b4abab0fd7daf351f841233
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-10-28 17:22:47 +0530

    Added a hardcoded image which will now be used to show the movie thumbnail.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import CardHeader from '@material-ui/core/CardHeader';
-import PropTypes from 'prop-types'; 
-import {Row, Col} from 'react-bootstrap'
+import Card from '@material-ui/core/Card';
+import PropTypes from 'prop-types';
+import CardMedia from '@material-ui/core/CardMedia'
 
+var styles = {
+  thumbnail : {
+    height: 100,
+    width: 100,
+    float: 'left',
+    margin: '-0.5em 1em 1.5em 0',
+    paddingBottom: '1em'
+  }
+}
 export class MovieCard extends React.Component {
   render() {
     return (
-        <CardHeader
-          title={this.props.movie.name}
-          subheader={this.props.movie.snippet}
+      <Card>
+        <CardMedia className="thumbnail"
+        title="Without Quote" style={styles.thumbnail}
+        image="https://images-na.ssl-images-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL50_.jpg"
         />
+        <CardHeader title={this.props.movie.name} subheader={this.props.movie.snippet} />
+      </Card>
     );
   }
 }
