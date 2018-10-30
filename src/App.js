7bccaaa27cde24ea61340e35d8c11e4d6e8f9f48
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-10-30 15:07:10 +0530

    App is now a function instead of extending component class.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import MovieListContainer from './containers/MovieListContainer';
 
-class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-          <MovieListContainer/>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <MovieListContainer />
+  </div>
+)
 
 export default App;
