fcc48a6c2553de8384eec83141ea264fba1aecb0
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-10-30 15:26:34 +0530

    Added a reserve button which does nothing.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,6 +3,7 @@ import CardHeader from '@material-ui/core/CardHeader';
 import Card from '@material-ui/core/Card';
 import PropTypes from 'prop-types';
 import CardMedia from '@material-ui/core/CardMedia'
+import { ReserveButton } from './ReserveButton';
 
 var styles = {
   thumbnail : {
@@ -20,6 +21,7 @@ export class MovieCard extends React.Component {
         <CardMedia className="thumbnail" title={this.props.movie.name} 
         style={styles.thumbnail} image={this.props.movie.imageUrl} />
         <CardHeader title={this.props.movie.name} subheader={this.props.movie.snippet} />
+        <ReserveButton />
       </Card>
     );
   }
---
+++ src/components/ReserveButton.js
@@ -0,0 +1,12 @@
+import Button from '@material-ui/core/Button';
+import React, { Component } from 'react';
+
+export class ReserveButton extends Component {
+
+  render() {
+    return (
+      <Button variant="contained" color="primary" >Reserve</Button>
+    );
+  }
+
+}
